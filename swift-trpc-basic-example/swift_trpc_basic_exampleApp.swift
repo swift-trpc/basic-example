39a07fb88d8ef030388639d99998b77b4e0e7f2d
Artem Tarasenko <shabashab.04@gmail.com>
2024-11-17 15:55:17 +0200

    Initial Commit

---
+++ swift-trpc-basic-example/swift_trpc_basic_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_trpc_basic_exampleApp.swift
+//  swift-trpc-basic-example
+//
+//  Created by Artem Tarasenko on 17.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct swift_trpc_basic_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
